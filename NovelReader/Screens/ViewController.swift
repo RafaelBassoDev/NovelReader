0cb5cd57872ef5f102252f19bd75b8ce58dcde35
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-28 17:05:28 -0300

    refactor(ViewController): added 'coordinator' attribute as weak var.

--- NovelReader/Screens/ViewController.swift
+++ NovelReader/Screens/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    weak var coordinator: Coordinator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
