eeaf5689ec8c72b5d1f6d4558f11a2867c64bc7d
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-28 17:23:23 -0300

    feat(Extensions/UINavigationController): implemented methods to set default appearence.

--- NovelReader/Extensions/UINavigationController.swift
+++ NovelReader/Extensions/UINavigationController.swift
@@ -5,4 +5,41 @@
 //  Created by Rafael Basso on 28/03/23.
 //
 
-import Foundation
+import UIKit.UINavigationController
+import UIKit.UINavigationBar
+
+extension UINavigationController {
+    func setCustomAppearance() {
+        navigationBar.setCustomCompactAppearance()
+        navigationBar.setCustomScrollEdgeAppearance()
+    }
+}
+
+extension UINavigationBar {
+    func setCustomCompactAppearance() {
+        let defaultAppearance = UINavigationBarAppearance()
+        
+        defaultAppearance.configureWithTransparentBackground()
+        defaultAppearance.backgroundColor = .black
+        defaultAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 22, weight: .bold)
+        ]
+        
+        self.standardAppearance = defaultAppearance
+        self.compactAppearance = defaultAppearance
+    }
+    
+    func setCustomScrollEdgeAppearance() {
+        let scrollAppearance = UINavigationBarAppearance()
+        
+        scrollAppearance.configureWithOpaqueBackground()
+        scrollAppearance.backgroundColor = .black
+        scrollAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 22, weight: .bold)
+        ]
+        
+        self.scrollEdgeAppearance = scrollAppearance
+    }
+}
