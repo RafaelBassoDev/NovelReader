9288e7b11207e4f36cf5373aef3f24b6b6c6abb0
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-28 17:04:50 -0300

    feat(Coordinator): implemented 'start' and 'pop' methods.

--- NovelReader/Services/Coordinator.swift
+++ NovelReader/Services/Coordinator.swift
@@ -5,4 +5,28 @@
 //  Created by Rafael Basso on 28/03/23.
 //
 
-import Foundation
+import UIKit
+
+final class Coordinator {
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = ViewController()
+        
+        viewController.coordinator = self
+        
+        viewController.title = "Novels"
+        
+        viewController.modalPresentationStyle = .fullScreen
+        
+        navigationController.pushViewController(viewController, animated: true)
+    }
+    
+    func pop() {
+        navigationController.popViewController(animated: true)
+    }
+}
