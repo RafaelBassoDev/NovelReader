d2a26a4f4b504914326794229c30100290041346
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-28 14:43:20 -0300

    Initial Commit

---
+++ NovelReaderUITests/NovelReaderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NovelReaderUITestsLaunchTests.swift
+//  NovelReaderUITests
+//
+//  Created by Rafael Basso on 28/03/23.
+//
+
+import XCTest
+
+final class NovelReaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
