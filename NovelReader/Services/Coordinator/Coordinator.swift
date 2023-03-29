f02114c068f95f494b007eb62def792545d1253b
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-29 11:12:02 -0300

    feat(Coordinator): collection view layout now defines custom size and insets for cells.

--- NovelReader/Services/Coordinator/Coordinator.swift
+++ NovelReader/Services/Coordinator/Coordinator.swift
@@ -18,6 +18,12 @@ final class Coordinator {
         
         let layout = UICollectionViewFlowLayout()
         
+        let width = 180
+        let height = 250
+        layout.itemSize = CGSize(width: width, height: height)
+        layout.estimatedItemSize = CGSize(width: width, height: height)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
         let viewController = NovelCollectionViewController(collectionViewLayout: layout)
         
         viewController.coordinator = self
