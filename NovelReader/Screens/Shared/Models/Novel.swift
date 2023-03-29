106d0d472fd5b22f196cd1e6b3c3d1168229ab26
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-29 09:53:14 -0300

    feat(Novel): created struct and its attributes.

--- NovelReader/Screens/Shared/Models/Novel.swift
+++ NovelReader/Screens/Shared/Models/Novel.swift
@@ -5,4 +5,10 @@
 //  Created by Rafael Basso on 29/03/23.
 //
 
-import Foundation
+import UIKit.UIImage
+
+struct Novel {
+    var poster: UIImage?
+    let title: String
+    var chapters: [Chapter]
+}
