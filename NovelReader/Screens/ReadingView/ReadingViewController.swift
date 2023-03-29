d3e978e2ab29864acd9581430415b4eb93bc5a54
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-29 18:57:13 -0300

    feat(ReadingViewController): embedded view on scroll view.

--- NovelReader/Screens/ReadingView/ReadingViewController.swift
+++ NovelReader/Screens/ReadingView/ReadingViewController.swift
@@ -7,9 +7,56 @@
 
 import UIKit
 
-class ReadingViewController: UIViewController {
+class ReadingViewController: UIViewController, Coordinatable {
+    weak var coordinator: Coordinator?
 
+    var chapter: Chapter!
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
+    
+    lazy var contentView: UIView = {
+        let view = UIView()
+        view.layoutMargins = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        return view
+    }()
+    
+    init(chapter: Chapter) {
+        self.chapter = chapter
+        super.init(nibName: nil, bundle: nil)
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(contentView)
+    }
+    
+    private func setConstraints() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
     }
 }
