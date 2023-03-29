f79f24364a851a0a62e0f6cefcaef9b4417beabc
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-29 18:47:54 -0300

    feat: added class 'ReadingViewController'.

---
+++ NovelReader/Screens/ReadingView/ReadingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ReadingViewController.swift
+//  NovelReader
+//
+//  Created by Rafael Basso on 29/03/23.
+//
+
+import UIKit
+
+class ReadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
