2e32539ab23e5d71ae7f044216316a7dd1e3f8a4
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-30 13:28:46 -0300

    refactor(ReadingViewController): added title and text views and fixed constraints.

--- NovelReader/Screens/ReadingView/ReadingViewController.swift
+++ NovelReader/Screens/ReadingView/ReadingViewController.swift
@@ -19,14 +19,49 @@ class ReadingViewController: UIViewController, Coordinatable {
     
     lazy var contentView: UIView = {
         let view = UIView()
-        view.layoutMargins = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        view.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         return view
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 38)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.sizeToFit()
+        return label
+    }()
+
+    lazy var contentText: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 22)
+        textView.textAlignment = .left
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    lazy var nextChapterButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.buttonSize = .medium
+        config.cornerStyle = .medium
+        config.title = "Next"
+        config.baseBackgroundColor = .green
+        
+        let button = UIButton(configuration: config)
+        button.addAction(
+            UIAction { _ in
+                print("Next")
+            },
+            for: .touchUpInside
+        )
+        return button
     }()
     
     init(chapter: Chapter) {
         self.chapter = chapter
         super.init(nibName: nil, bundle: nil)
-        setConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -36,9 +71,18 @@ class ReadingViewController: UIViewController, Coordinatable {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        titleLabel.text = chapter.title
+        contentText.text = chapter.content
+        
         view.addSubview(scrollView)
         
         scrollView.addSubview(contentView)
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(contentText)
+        contentView.addSubview(nextChapterButton)
+        
+        setConstraints()
     }
     
     private func setConstraints() {
@@ -57,6 +101,29 @@ class ReadingViewController: UIViewController, Coordinatable {
             contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
             contentView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor)
+        ])
+        
+        contentText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentText.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            contentText.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            contentText.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
+        ])
+        
+        nextChapterButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nextChapterButton.topAnchor.constraint(equalTo: contentText.bottomAnchor, constant: 60),
+            nextChapterButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            nextChapterButton.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: -40),
+            nextChapterButton.widthAnchor.constraint(equalToConstant: 120),
+            nextChapterButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
