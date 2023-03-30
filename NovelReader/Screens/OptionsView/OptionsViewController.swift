b91ab7feb83bd4a2015e71b61d7ede89ca33b253
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-30 14:54:31 -0300

    feat: created file 'OptionsViewController'.

---
+++ NovelReader/Screens/OptionsView/OptionsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OptionsViewController.swift
+//  NovelReader
+//
+//  Created by Rafael Basso on 30/03/23.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
