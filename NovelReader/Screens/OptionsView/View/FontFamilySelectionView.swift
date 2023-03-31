7ad6641bc7c14c42bda73bb2c8dbc43b78456eb7
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-31 12:52:07 -0300

    feat: created file 'FontFamilySelectionView'.

---
+++ NovelReader/Screens/OptionsView/View/FontFamilySelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  FontFamilySelectionView.swift
+//  NovelReader
+//
+//  Created by Rafael Basso on 31/03/23.
+//
+
+import UIKit
+
+class FontFamilySelectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
