0bb699585235e5ff3adc29cb8c48ceefb6dc5836
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-31 12:53:11 -0300

    feat(OptionsViewController): implemented table view behavior and custom cells.

--- NovelReader/Screens/OptionsView/OptionsViewController.swift
+++ NovelReader/Screens/OptionsView/OptionsViewController.swift
@@ -7,9 +7,67 @@
 
 import UIKit
 
-class OptionsViewController: UIViewController {
+class OptionsViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private enum FontSettings: String, CaseIterable {
+        case fontFamily = "Font Family"
+        case fontSize = "Font Size"
+    }
+    
+    private let options: [FontSettings] = [
+        .fontFamily,
+        .fontSize
+    ]
+    
+    init() {
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
+
+extension OptionsViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch options[indexPath.row] {
+        case .fontFamily:
+            print("font family")
+            
+        case .fontSize:
+            print("font size")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.selectionStyle = .gray
+        cell.accessoryType = .disclosureIndicator
+        
+        let backgroundConfiguration = UIBackgroundConfiguration.listPlainCell()
+        cell.backgroundConfiguration = backgroundConfiguration
+        
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = options[indexPath.row].rawValue
+        contentConfiguration.textProperties.font = UIFont.boldSystemFont(ofSize: 20)
+        contentConfiguration.textProperties.lineBreakMode = .byTruncatingTail
+        contentConfiguration.textProperties.numberOfLines = 2
+        cell.contentConfiguration = contentConfiguration
+        
+        return cell
     }
 }
