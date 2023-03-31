f230c5ca7a8640d6a75effa7fd84daef0f025326
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-31 13:14:44 -0300

    feat(FontFamilySelectionView): implemented table view logic and cell styling.

--- NovelReader/Screens/OptionsView/View/FontFamilySelectionView.swift
+++ NovelReader/Screens/OptionsView/View/FontFamilySelectionView.swift
@@ -7,14 +7,58 @@
 
 import UIKit
 
-class FontFamilySelectionView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class FontFamilySelectionView: UITableViewController {
+    
+    var fontOptions: [String]!
+    
+    init(fontOptions: [String] = []) {
+        self.fontOptions = [
+            "SFPro",
+            "Arial",
+            "Comic Sans",
+            "Times New York"
+        ]
+        super.init(style: .plain)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
 
+extension FontFamilySelectionView {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fontOptions.count
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected font: \(fontOptions[indexPath.row])")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.selectionStyle = .gray
+        
+        let backgroundConfiguration = UIBackgroundConfiguration.listPlainCell()
+        cell.backgroundConfiguration = backgroundConfiguration
+        
+        let selectedFontName = fontOptions[indexPath.row]
+        
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = selectedFontName
+        contentConfiguration.textProperties.font = UIFont(name: selectedFontName, size: 17) ?? UIFont.systemFont(ofSize: 17, weight: .regular)
+        cell.contentConfiguration = contentConfiguration
+        
+        return cell
+    }
 }
