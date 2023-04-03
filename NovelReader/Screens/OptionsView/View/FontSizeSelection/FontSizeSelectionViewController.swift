2cccb1b510aff679b73f28181fac42cfc184449c
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-03 16:06:34 -0300

    feat(FontSizeSelectionViewController): created reference text UILabel and added constraints.

--- NovelReader/Screens/OptionsView/View/FontSizeSelection/FontSizeSelectionViewController.swift
+++ NovelReader/Screens/OptionsView/View/FontSizeSelection/FontSizeSelectionViewController.swift
@@ -9,4 +9,38 @@ import UIKit
 
 class FontSizeSelectionViewController: UIViewController {
     
+    lazy var referenceText: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.text = "The quick brown fox jumps over the lazy dog."
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = .byTruncatingTail
+        label.sizeToFit()
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Font Size"
+        
+        view.addSubview(referenceText)
+        
+        setupConstraints()
+    }
+}
+
+extension FontSizeSelectionViewController {
+    private func setupConstraints() {
+        
+        referenceText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            referenceText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            referenceText.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            referenceText.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+    }
 }
