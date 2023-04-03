d463d7ab270296e8ddde3631d258d3afa29dd276
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-03 18:14:36 -0300

    feat(FontSizeSelectionViewController): added slider view and implemented constraints.

--- NovelReader/Screens/OptionsView/View/FontSizeSelection/FontSizeSelectionViewController.swift
+++ NovelReader/Screens/OptionsView/View/FontSizeSelection/FontSizeSelectionViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class FontSizeSelectionViewController: UIViewController {
     
-    lazy var referenceText: UILabel = {
+    private(set) lazy var referenceText: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 22)
         label.text = "The quick brown fox jumps over the lazy dog."
@@ -21,12 +21,21 @@ class FontSizeSelectionViewController: UIViewController {
         return label
     }()
     
+    private(set) lazy var sliderView: SizeSliderView = {
+        let sliderView = SizeSliderView()
+        sliderView.backgroundColor = .darkGray.withAlphaComponent(0.3)
+        sliderView.layer.borderWidth = 0.2
+        sliderView.layer.borderColor = UIColor.gray.cgColor
+        return sliderView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Font Size"
         
         view.addSubview(referenceText)
+        view.addSubview(sliderView)
         
         setupConstraints()
     }
@@ -34,7 +43,6 @@ class FontSizeSelectionViewController: UIViewController {
 
 extension FontSizeSelectionViewController {
     private func setupConstraints() {
-        
         referenceText.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             referenceText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
@@ -42,5 +50,11 @@ extension FontSizeSelectionViewController {
             referenceText.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
         
+        sliderView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            sliderView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            sliderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            sliderView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 }
