ddabb9eef83d534c4da6645d259de62d67a1ea14
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-06 15:41:46 -0300

    refactor(Coordinator): class now uses a repository instead of local variables.

--- NovelReader/Services/Coordinator/Coordinator.swift
+++ NovelReader/Services/Coordinator/Coordinator.swift
@@ -10,12 +10,15 @@ import UIKit
 final class Coordinator {
     var navigationController: UINavigationController
     
-    init(navigationController: UINavigationController) {
+    var novelRepository: NovelRepositoreable
+    
+    init(navigationController: UINavigationController, repository: NovelRepositoreable) {
         self.navigationController = navigationController
+        self.novelRepository = repository
     }
     
     func start() {
-        let viewModel = NovelCollectionViewModel()
+        let viewModel = NovelCollectionViewModel(novels: novelRepository.dataSource)
         let viewController = NovelCollectionViewController(viewModel: viewModel)
         viewController.coordinator = self
         viewController.title = "Novels"
@@ -27,7 +30,9 @@ final class Coordinator {
     func pop() {
         navigationController.popViewController(animated: true)
     }
-    
+}
+
+extension Coordinator {
     func showChapterList(for novel: Novel) {
         let viewModel = ChapterListViewModel(chapters: novel.chapters)
         let viewController = ChapterListViewController(viewModel: viewModel)
@@ -48,6 +53,11 @@ final class Coordinator {
         let viewController = SettingsViewController()
         viewController.title = "Settings"
         
+        navigationController.pushViewController(viewController, animated: true)
+    }
+    
+    func showNextChapter() {
+        let viewController = ReadingViewController(chapter: novelRepository.dataSource[0].chapters[0])
         navigationController.pushViewController(viewController, animated: true)
     }
 }
