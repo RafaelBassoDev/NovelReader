76db33f7db8a616a9f8704bba2cd1031a91e2c04
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-06 16:35:31 -0300

    feat(NovelRepository): implemented protocol methods.

--- NovelReader/Services/Repositories/NovelRepository.swift
+++ NovelReader/Services/Repositories/NovelRepository.swift
@@ -7,8 +7,47 @@
 
 import Foundation
 
-class NovelRepository {
-    init() {
+class NovelRepository: NovelRepositoreable {
+    private(set) var novels: [Novel]
+    private(set) var currentNovel: Novel?
+    private(set) var currentChapter: Chapter?
+    
+    var currentChapterIndex: Int? {
+        guard let novelChapters = currentNovel?.chapters, let currentChapter else { return nil }
+        if novelChapters.isEmpty {
+            return nil
+        }
+        return novelChapters.firstIndex(of: currentChapter)
+    }
+    
+    init(novels: [Novel]) {
+        self.novels = novels
+    }
+}
+
+extension NovelRepository {
+    func setCurrentNovel(_ novel: Novel) {
+        self.currentNovel = novel
+    }
+    
+    func getNextChapter() -> Chapter? {
+        let chapter = searchNextChapter()
+        if chapter != nil {
+            self.currentChapter = chapter
+        }
+        return chapter
+    }
+    
+    private func searchNextChapter() -> Chapter? {
+        guard let currentNovelChapters = currentNovel?.chapters else { return nil }
+        guard let currentChapterIndex else { return nil }
         
+        let lastChapterIndex = currentNovelChapters.count - 1
+        
+        guard currentChapterIndex < lastChapterIndex else { return nil }
+        
+        let nextChapterIndex = currentChapterIndex + 1
+        
+        return currentNovelChapters[nextChapterIndex]
     }
 }
