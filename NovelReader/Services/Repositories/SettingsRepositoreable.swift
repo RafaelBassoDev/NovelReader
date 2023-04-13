75d7c978361863df0c710f426931dd9898cc529e
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-13 17:18:21 -0300

    feat: created protocol 'SettingsRepositoreable'.

---
+++ NovelReader/Services/Repositories/SettingsRepositoreable.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsRepositoreable.swift
+//  NovelReader
+//
+//  Created by Rafael Basso on 13/04/23.
+//
+
+import Foundation
--- NovelReader/Services/Repositories/SettingsRepository.swift
+++ NovelReader/Services/Repositories/SettingsRepository.swift
@@ -5,4 +5,13 @@
 //  Created by Rafael Basso on 13/04/23.
 //
 
-import Foundation
+import UIKit
+
+class SettingsRepository {
+    // saveSettings
+    // updateSettings
+    
+    func getLatestUIFontSettings() -> UIFont {
+        return UIFont.systemFont(ofSize: CGFloat.random(in: 12...60), weight: .regular)
+    }
+}
