2b77afa1da33f7c670bc03ee034252639925ce9f
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-18 16:34:25 -0300

    style(SettingsRepository): moved protocol conformation to extension.

--- NovelReader/Services/Repositories/SettingsRepository/SettingsRepository.swift
+++ NovelReader/Services/Repositories/SettingsRepository/SettingsRepository.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingsRepository: SettingsRepositoreable {
+class SettingsRepository {
     private var availableFontFamilies = [
         "SFPro",
         "Arial",
@@ -27,7 +27,7 @@ class SettingsRepository: SettingsRepositoreable {
     }
 }
 
-extension SettingsRepository {
+extension SettingsRepository: SettingsRepositoreable {
     func getAvailableFontFamilyNames() -> [String] {
         return availableFontFamilies
     }
