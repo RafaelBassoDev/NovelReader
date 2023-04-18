3e654cf96a7abc13e82ed5871717d56e07159bb5
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-18 16:01:55 -0300

    feat: created 'SettingsStorage' and 'SettingsStorable'.

---
+++ NovelReader/Services/Repositories/SettingsRepository/SettingsStorable.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsStorable.swift
+//  NovelReader
+//
+//  Created by Rafael Basso on 18/04/23.
+//
+
+import Foundation
+
+protocol SettingsStorable: AnyObject {
+    
+}
---
+++ NovelReader/Services/Repositories/SettingsRepository/SettingsStorage.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsStorage.swift
+//  NovelReader
+//
+//  Created by Rafael Basso on 18/04/23.
+//
+
+import Foundation
+
+class SettingsStorage: SettingsStorable {
+    
+}
