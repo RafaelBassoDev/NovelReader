4b9cd21a52fb22745770f07d24725bfc6eb05378
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-20 22:34:08 -0300

    feat(NovelStorage): added default implementation of 'NovelStorable' protocol methods.

--- NovelReader/Services/Storage/Novels/NovelStorage.swift
+++ NovelReader/Services/Storage/Novels/NovelStorage.swift
@@ -9,4 +9,31 @@ import Foundation
 
 class NovelStorage: NovelStorable {
     
+    private var dataSource: [Chapter] = {
+        var chapters = [Chapter]()
+        for num in 1...5 {
+            chapters.append(Chapter(number: num, title: "Chapter \(num)", subtitle: "The subtitle of chapter \(num)"))
+        }
+        return chapters
+    }()
+    
+    func getStoredNovels() async -> [Novel] {
+        return [
+            Novel(title: "A Will Eternal"),
+            Novel(title: "Immortal Renagate")
+        ]
+    }
+    
+    func getStoredChapters(of novel: Novel) async -> [Chapter] {
+        return dataSource
+    }
+    
+    // Chapter without content
+    func getStoredChapter(number: Int) async -> Chapter? {
+        return dataSource[number - 1]
+    }
+    
+    func getStoredContent(of chapter: Chapter) async -> String {
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo."
+    }
 }
