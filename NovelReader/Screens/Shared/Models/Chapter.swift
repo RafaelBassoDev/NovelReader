569473fbe6f72b7142a18314f6d4c825dd928998
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-20 22:32:05 -0300

    refactor(Chapter): moved Equatable implementation to extension and added 'number' attribute.

--- NovelReader/Screens/Shared/Models/Chapter.swift
+++ NovelReader/Screens/Shared/Models/Chapter.swift
@@ -7,20 +7,28 @@
 
 import Foundation
 
-struct Chapter: Identifiable, Equatable {
-    var id: UUID
+struct Chapter {
+    let number: Int
     let title: String
     let subtitle: String?
     let content: String
-
-    init(title: String, subtitle: String? = nil, content: String) {
-        self.id = UUID()
+    
+    init(number: Int, title: String, subtitle: String? = nil, content: String = "") {
+        self.number = number
         self.title = title
         self.subtitle = subtitle
         self.content = content
     }
+}
 
+extension Chapter {
+    func withContent(_ newContent: String) -> Self {
+        return Chapter(number: number, title: title, subtitle: subtitle, content: newContent)
+    }
+}
+
+extension Chapter: Equatable {
     static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.id == rhs.id
+        return lhs.number == rhs.number
     }
 }
