1c09064a18bbd8c4d5f14afff084a2464667c1c1
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-22 13:26:40 -0300

    feat(LoadingViewController): implemented subviews and constraints.

--- NovelReader/Screens/LoadingView/LoadingViewController.swift
+++ NovelReader/Screens/LoadingView/LoadingViewController.swift
@@ -11,55 +11,87 @@ class LoadingViewController: UIViewController {
 
     private(set) lazy var loadingindicator: UIActivityIndicatorView = {
         let loadingView = UIActivityIndicatorView(style: .large)
-        
-        loadingView.style = .large
         loadingView.color = .white
-        
         loadingView.startAnimating()
-        
-        loadingView.autoresizingMask = [
-            .flexibleLeftMargin, .flexibleRightMargin,
-            .flexibleTopMargin, .flexibleBottomMargin
-        ]
-        
         return loadingView
     }()
     
+    private(set) lazy var loadingText: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        label.text = "Loading..."
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        label.textColor = .white
+        label.sizeToFit()
+        return label
+    }()
+    
+    private(set) lazy var loadingBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.layer.borderColor = UIColor.gray.cgColor
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
     private(set) lazy var blurEffectView: UIVisualEffectView = {
-        let effect = UIBlurEffect(style: .regular)
+        let effect = UIBlurEffect(style: .dark)
         let view = UIVisualEffectView(effect: effect)
-        
         view.alpha = 0.5
-        
-        view.autoresizingMask = [
-            .flexibleWidth,
-            .flexibleHeight
-        ]
-        
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .black.withAlphaComponent(0.5)
+        view.backgroundColor = .black.withAlphaComponent(0.2)
         
-        setBlurEffect()
-        setLoadingIndicator()
+        view.insertSubview(blurEffectView, at: 0)
+        
+        view.addSubview(loadingBackground)
+        
+        loadingBackground.addSubview(loadingindicator)
+        loadingBackground.addSubview(loadingText)
+        
+        setConstraints()
     }
 }
 
 extension LoadingViewController {
-    private func setBlurEffect() {
-        blurEffectView.frame = view.bounds
-        view.insertSubview(blurEffectView, at: 0)
+    private func setConstraints() {
+        
+        setBlurEffectView()
+        
+        loadingBackground.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loadingBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loadingBackground.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loadingBackground.widthAnchor.constraint(equalToConstant: view.frame.width * 0.4),
+            loadingBackground.heightAnchor.constraint(equalToConstant: view.frame.width * 0.4)
+        ])
+        
+        loadingindicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loadingindicator.centerXAnchor.constraint(equalTo: loadingBackground.centerXAnchor),
+            loadingindicator.centerYAnchor.constraint(equalTo: loadingBackground.centerYAnchor, constant: (loadingindicator.frame.height * 0.5) * -1)
+        ])
+
+        loadingText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loadingText.topAnchor.constraint(equalTo: loadingindicator.bottomAnchor, constant: 15),
+            loadingText.centerXAnchor.constraint(equalTo: loadingindicator.centerXAnchor)
+        ])
     }
     
-    private func setLoadingIndicator() {
-        loadingindicator.center = CGPoint(
-            x: view.bounds.midX,
-            y: view.bounds.midY
-        )
-        view.addSubview(loadingindicator)
+    private func setBlurEffectView() {
+        blurEffectView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            blurEffectView.topAnchor.constraint(equalTo: view.topAnchor),
+            blurEffectView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            blurEffectView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurEffectView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
     }
 }
