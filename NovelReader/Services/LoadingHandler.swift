3a8d09781948b22ba8adaf275ef60b21116e0c36
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-04-22 13:25:51 -0300

    feat(LoadingHandler): implemented methods to start and stop loading.

--- NovelReader/Services/LoadingHandler.swift
+++ NovelReader/Services/LoadingHandler.swift
@@ -5,4 +5,32 @@
 //  Created by Rafael Basso on 22/04/23.
 //
 
-import Foundation
+import UIKit
+
+struct LoadingHandler {
+    
+    private let loadingController: LoadingViewController = {
+        let controller = LoadingViewController()
+        controller.modalPresentationStyle = .overCurrentContext
+        controller.modalTransitionStyle = .crossDissolve
+        return controller
+    }()
+    
+    private weak var navigationController: UINavigationController?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func startLoading() {
+        DispatchQueue.main.async {
+            self.navigationController?.present(loadingController, animated: true)
+        }
+    }
+    
+    func stopLoading() {
+        DispatchQueue.main.async {
+            self.loadingController.dismiss(animated: true)
+        }
+    }
+}
