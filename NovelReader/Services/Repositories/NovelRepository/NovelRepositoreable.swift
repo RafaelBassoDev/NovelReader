22771c1ad76f51e7e4cfe5b1472fa037c6914691
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-05-11 15:47:16 -0300

    feat(NovelRepositoreable): added new method 'getChapter'.

--- NovelReader/Services/Repositories/NovelRepository/NovelRepositoreable.swift
+++ NovelReader/Services/Repositories/NovelRepository/NovelRepositoreable.swift
@@ -10,7 +10,7 @@ import Foundation
 protocol NovelRepositoreable {
     func getStoredNovels() async -> [NovelModel]
     func getStoredChapters(of novel: NovelModel) async -> [ChapterModel]
+    func getChapter(number: Int) async -> ChapterModel?
     func getNextChapter() async -> ChapterModel?
     func getPreviousChapter() async -> ChapterModel?
-    func getChapterContent(_ chapter: ChapterModel) async -> String
 }
