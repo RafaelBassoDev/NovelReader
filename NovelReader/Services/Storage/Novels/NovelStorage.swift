e3a0a6d989c01bdd5b80ed3d6120ce8183846e50
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-05-11 15:50:00 -0300

    feat(NovelStorage): implemented core data fetching logic.

--- NovelReader/Services/Storage/Novels/NovelStorage.swift
+++ NovelReader/Services/Storage/Novels/NovelStorage.swift
@@ -5,42 +5,82 @@
 //  Created by Rafael Basso on 20/04/23.
 //
 
-import Foundation
+import UIKit
+import CoreData
 
 class NovelStorage: NovelStorable {
-    
-    private var dataSource: [ChapterModel] = {
-        var chapters = [ChapterModel]()
-        for num in 1...5 {
-            chapters.append(ChapterModel(number: num, title: "Chapter \(num)", subtitle: "The subtitle of chapter \(num)"))
-        }
-        return chapters
+
+    // swiftlint:disable force_cast
+    private let managedContext: NSManagedObjectContext = {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
     }()
+    // swiftlint:enable force_cast
     
     func getStoredNovels() async -> [NovelModel] {
-        return [
-            NovelModel(title: "A Will Eternal"),
-            NovelModel(title: "Immortal Renagate")
-        ]
+        var storedNovels = [NovelModel]()
+        
+        do {
+            let request = NSFetchRequest<NSManagedObject>(entityName: "Novel")
+            let fetchedNovels = try managedContext.fetch(request)
+            
+            for novel in fetchedNovels {
+                guard let title = novel.value(forKey: "title") as? String else { continue }
+//                let poster = novel.value(forKey: "poster")
+                let model = NovelModel(title: title)
+                storedNovels.append(model)
+            }
+        } catch {
+            print("ERROR::[NovelStorage]-getStoredNovels")
+            print(error)
+        }
+            
+        return storedNovels
     }
     
     func getStoredChapters(of novel: NovelModel) async -> [ChapterModel] {
-        return dataSource
+        var storedChapters = [ChapterModel]()
+        
+        do {
+            let request = NSFetchRequest<NSManagedObject>(entityName: "Chapter")
+            request.predicate = NSPredicate(format: "novel.title == %@", novel.title)
+            
+            let fetchedChapters = try managedContext.fetch(request)
+            
+            for chapter in fetchedChapters {
+                guard let number = chapter.value(forKey: "number") as? Int else { continue }
+                guard let title = chapter.value(forKey: "title") as? String else { continue }
+                let subtitle = chapter.value(forKey: "subtitle") as? String
+                
+                let model = ChapterModel(number: number, title: title, subtitle: subtitle)
+                storedChapters.append(model)
+            }
+        } catch {
+            print("ERROR::[NovelStorage]-getStoredChapters(novel:_)")
+            print(error)
+        }
+        
+        return storedChapters.sorted { $0.number < $1.number }
     }
     
-    // Chapter without content
-    func getStoredChapter(number: Int) async -> ChapterModel? {
-        return dataSource[number - 1]
-    }
-    
-    // swiftlint:disable all
-    func getStoredContent(of chapter: ChapterModel) async -> String {
-        return """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id.
-        
-        Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id.
-        
-        Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra bibendum libero, quis sollicitudin nibh eleifend id. Pellentesque at iaculis leo.
-        """
+    func getStoredChapter(of novel: NovelModel, number: Int) async -> ChapterModel? {
+        var fetchedChapter: ChapterModel?
+        do {
+            let request = NSFetchRequest<NSManagedObject>(entityName: "Chapter")
+            request.predicate = NSPredicate(format: "novel.title == %@ AND number == %d", novel.title, number)
+            
+            if let chapter = try managedContext.fetch(request).first {
+                guard let number = chapter.value(forKey: "number") as? Int else { return nil }
+                guard let title = chapter.value(forKey: "title") as? String else { return nil }
+                guard let content = chapter.value(forKey: "content") as? String else { return nil }
+                let subtitle = chapter.value(forKey: "subtitle") as? String
+
+                fetchedChapter = ChapterModel(number: number, title: title, subtitle: subtitle, content: content)
+            }
+        } catch {
+            print("ERROR::[NovelStorage]-getStoredChapter(novel:_, number:_)")
+            print(error)
+        }
+        return fetchedChapter
     }
 }
